0e9b931fa0306272d5cb234cfce5f8b548f8efc2
Danijel B <danijel.balaz1@gmail.com>
2022-07-27 22:46:53 +0200

    Add number input

--- javascript.js
+++ javascript.js
@@ -24,16 +24,18 @@ function operate(operator, a, b) {
 
 //DOM 
 
-const displayInput = document.querySelector('.input-display');
+const displayInput = document.getElementById('display');
 
 const numbers = document.querySelectorAll('.number');
 
 numbers.forEach((number) => {
-    number.addEventListener('click', () => {
-        displayInput.innerHTML = number.value
-        console.log(number.value)
+    number.addEventListener('click', function() {
+        displayInput.value += this.value;
+        displayInput.innerHTML = displayInput.value;
+        console.log(displayInput.value);
     })
 })
+
 
 
 console.log(operate('-', 15, 7));
